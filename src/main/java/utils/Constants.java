2746b3be42c832cfd8b41a15ebc9b8624c16d41c
Viktoriia Zhaba <vzhaba3105@gmail.com>
2022-11-11 00:41:38 +0200

    Initi commit

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,34 @@
+package utils;
+
+public class Constants {
+    public static final String UNKNOWN_MSG= "Unknown";
+    public static final String FOR_PAYMENT_MSG="Your amount to be paid is ";
+    public static final String FOR_BONUS_MSG="Ваша виплата з бонусом становить: ";
+    public static final String RED_MSG="Stand up!";
+    public static final String YELLOW_MSG="Get ready to move!";
+    public static final String GREEN_MSG="Go!";
+    public static final String QUESTION_MSG= """
+            What color traffic light do you see in front of you?
+            1. Red;
+            2. Yellow;
+            3. Green;
+            """;
+    public static final String POINT_MSG= """
+            Hello, dear employees!
+                Let's analyze this month customers' feedbacks!
+                Enter the rate seen in the feedback,
+                where:
+                1 - very bad
+                2 - bad
+                3 - good
+                4 - very good
+                5 - excellent
+            """;
+    public static final String RATE1_MSG="Very bad! We should start remaking the product!";
+    public static final String RATE2_MSG="Bad. Next product should be better";
+    public static final String RATE3_MSG="Good. This month we get to get a salary xD";
+    public static final String RATE4_MSG="Very good! Look who's getting a bonus this month!";
+    public static final String RATE5_MSG="Excellent! +10 days to this year's vacation!";
+    public static final String RATE_UNKNOWN_MSG="We don't have such a rate system. Try again!";
+    public static final String RTG_MSG="Product rating: ";
+}
